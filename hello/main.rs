576bcdcc1aca004891cf97bc75a4b69c176c1c5a
Xuan Lin <xuanlin@Xuans-MacBook-Pro.local>
2024-01-24 20:35:41 -0800

    basic loop, add things up

--- hello/main.rs
+++ hello/main.rs
@@ -1,5 +1,12 @@
 mod mod1;
 mod mod2;
+fn add_things() -> u32{
+    let mut sumi:u32 = 0;
+    for i in 1..101 { //upper bound is exclusive, lower bound is inclusive
+        sumi += i;
+    }
+    sumi as u32
+}
 
 fn main() {
     println!("Hello Sean!");
@@ -7,4 +14,7 @@ fn main() {
     // mod1::fn1(); // error: fn1 is private
     mod2::fnmod2();
     mod2::another_mod::another();
+
+    //basic loop, add 1 to 100
+    println!("sum: {:?}", add_things());
 }
